1c9a740546ddc453ea66ed848b2ab8f2a79e2645
vanessa leme <80298199+agenciapublion@users.noreply.github.com>
2021-10-14 21:22:50 -0300

    dkjfjsg
    saf

---
+++ src/pages/api/logout.js
@@ -0,0 +1,8 @@
+
+export default function handler(req, res) {
+  if (req.method === 'DELETE') {
+    // Process a DELETE request
+  } else {
+    // Handle any other HTTP method
+  }
+}
---
+++ src/pages/api/pergevent.js
@@ -0,0 +1,3 @@
+export default function handler(req, res) {
+  res.status(200).json({ pergunta: 'o evento vai ser fecahdo para muitas pessoas?' })
+}
